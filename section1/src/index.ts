c8cea328f3b1403c38ea7451cffa306549d9be5f
siltea2019 <siltea2019@gmail.com>
2025-02-18 23:33:40 +0900

    수업 실습: section1 - tsconfig.json 설정

---
+++ section1/src/index.ts
@@ -0,0 +1,14 @@
+import { hello } from './hello';
+
+console.log('Hello TypeScript');
+const a: number = 1;
+
+hello('siltea');
+
+// test.ts
+console.log('Hello Test');
+
+// target test
+const func = () => {
+	console.log('Hello');
+};
