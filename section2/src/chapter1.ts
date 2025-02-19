edeae88b39f91ce4869b86a134ab1ff6fd7dea83
siltea2019 <siltea2019@gmail.com>
2025-02-19 18:17:33 +0900

    수업실습: section2 - 배열과 튜플

--- section2/src/chapter1.ts
+++ section2/src/chapter1.ts
@@ -7,26 +7,15 @@ let num5: number = Infinity;
 let num6: number = -Infinity;
 let num7: number = NaN;
 
-num1 = 'hello'; // 문자타입 할당 X
-num1.toUpperCase(); // 문자타입 메서드 사용 X
-num1.toFixed(); // 숫자타입 메서드 사용 O
-
 // string
 let str1: string = 'hello';
 let str2: string = 'hello';
 let str3: string = `hello`;
 let str4: string = `hello ${num1}`;
 
-str1 = 123; // 숫자타입 할당 X
-str1.toFixed(); // 숫자타입 메서드 사용 X
-str1.toUpperCase(); // 문자타입 메서드 사용 O
-
 // boolean
 let bool1: boolean = true;
 let bool2: boolean = false;
-
-bool1 = 123;
-bool1 = 'boolean';
 
 // null
 let null1: null = null;
@@ -35,14 +24,11 @@ let null1: null = null;
 let unde1: undefined = undefined;
 
 // "strictNullChecks": false
-let numStrictNull: number = null;
+// let numStrictNull: number = null;
 
 // 리터럴 타입 - 리터럴 : 값
 let numA: 10 = 10;
-numA = 12;
 
 let strA: 'hello' = 'hello';
-strA = 'bye';
 
 let boolA: true = true;
-boolA = false;
---
+++ section2/src/chapter2.ts
@@ -0,0 +1,40 @@
+// 배열
+let numArrL: number[] = [1, 2, 3];
+
+let strArr: string[] = ['Hello', "i'm", 'siltea'];
+
+let boolArr: Array<boolean> = [true, false, true];
+
+// 배열에 들어가는 요소들의 타입이 다양할 경우
+// Hint! TS의 자동추론 이용하기
+let multiArr: (number | string)[] = [1, 'hello'];
+
+// 다차원 배열의 타입을 정의 하는 방법
+let duoubleArr: number[][] = [
+	[1, 2, 3],
+	[4, 5],
+];
+
+// 튜플
+// 길이와 타입이 고정된 배열
+let tup1: [number, number] = [1, 2];
+tup1 = [1, 2, 3];
+tup1 = ['1', '2'];
+
+let tup2: [number, string, boolean] = [1, '2', true];
+tup2 = ['2', 1, false];
+tup2 = [1];
+
+// 배열 메서드도 자유롭게 사용이 가능하다
+tup1.push(1);
+tup1.pop();
+
+// 튜플의 사용
+const users: [string, number][] = [
+	['김수로왕', 1],
+	['박혁거세', 2],
+	['왕건', 3],
+	['고주몽', 4],
+	['온조', 5],
+	[6, '단군왕검'],
+];
