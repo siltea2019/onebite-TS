0976b61aee531f0785f2905cfe114e265fa92189
Sumin Kim <siltea2019@gmail.com>
2025-03-01 23:45:08 +0900

    수업실습: section5 - 인터페이스

---
+++ section5/chapter2.ts
@@ -0,0 +1,33 @@
+// 선언 합침
+
+interface Person {
+    name: string;
+}
+
+interface Person {
+    age: number;
+}
+
+
+const person:Person = {
+    name: "",
+    age: 30,
+}
+
+
+// 모듈 보강
+
+interface Lib {
+    a: number;
+    b: number;
+}
+
+interface Lib {
+    c: string;
+}
+
+const lib = {
+    a:1,
+    b:2,
+    c:"Hi"
+}
